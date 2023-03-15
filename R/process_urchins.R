bcba956f91ebe6adfa303526eac1f48651f63ca7
hannahbarkley <38119916+hannahbarkley@users.noreply.github.com>
2023-03-14 16:07:27 -1000

    More update and changes

--- R/process_urchins.R
+++ R/process_urchins.R
@@ -3,8 +3,6 @@
 #'@author Hannah Barkley
 #'
 #'@param data Urchin observation data set.
-#'@param transect_id String of transect names (e.g., ("A1", "A2", "A3", "B1", "B2", "B3")). Defaults to NULL.
-#'@param transect_length String of transect lengths in meters (e.g., c(10, 10, 10, 10, 10, 10)). Defaults to NULL.
 #'@param method_name Transect design by which data were collected ("IPRB" or "Chords").
 #'
 #'@import tidyr
@@ -28,24 +26,17 @@
 #' )
 
 process_urchins <- function(data,
-                            transect_id = NULL,
                             transect_length = NULL,
                             method_name = c("IPRB", "Chords"),
                             full_summary = TRUE) {
-
   options(dplyr.summarise.inform = FALSE)
 
   # Create data frame with transect info
-  if (is.null(transect_id) == TRUE &
-      is.null(transect_length) == TRUE) {
-    transect_summary_pairs <-
-      unique(data[c("CB_TRANSECTID", "TRANSECT_LENGTH_M")])
-    transect_id <- unique(transect_summary_pairs$CB_TRANSECTID)
-    transect_length <-
-      unique(transect_summary_pairs$TRANSECT_LENGTH_M )
-  }
-    summary_transect <- data.frame(transect_id, transect_length)
+  data$OCC_SITEID_TRANSECT <-
+    paste0(data$OCC_SITEID, "-", data$CB_TRANSECTID)
 
+  summary_transect <-
+    unique(data[c("OCC_SITEID_TRANSECT", "TRANSECT_LENGTH_M")])
 
   # Convert to long format
   data_long <- data %>%
@@ -67,6 +58,7 @@ process_urchins <- function(data,
         "LOCALDATE",
         "CB_METHOD",
         "CB_TRANSECTID",
+        "OCC_SITEID_TRANSECT",
         "TRANSECT_LENGTH_M",
         "TAXON_NAME",
         "TAXON_CODE"
@@ -87,18 +79,20 @@ process_urchins <- function(data,
                  "TRGR",
                  "HEMA",
                  "MEGL")
-  all_bins <- c("TEST_SIZE_BIN_0_20_MM",
-                "TEST_SIZE_BIN_0_20_MM",
-                "TEST_SIZE_BIN_41_60_MM",
-                "TEST_SIZE_BIN_61_80_MM",
-                "TEST_SIZE_BIN_81_100_MM",
-                "TEST_SIZE_BIN_101_120_MM",
-                "TEST_SIZE_BIN_121_140_MM",
-                "TEST_SIZE_BIN_141_160_MM")
+  all_bins <- c(
+    "TEST_SIZE_BIN_0_20_MM",
+    "TEST_SIZE_BIN_0_20_MM",
+    "TEST_SIZE_BIN_41_60_MM",
+    "TEST_SIZE_BIN_61_80_MM",
+    "TEST_SIZE_BIN_81_100_MM",
+    "TEST_SIZE_BIN_101_120_MM",
+    "TEST_SIZE_BIN_121_140_MM",
+    "TEST_SIZE_BIN_141_160_MM"
+  )
 
   full_table <- expand.grid(
     OCC_SITEID = unique(data$OCC_SITEID),
-    CB_TRANSECTID = transect_id,
+    CB_TRANSECTID = unique(data$CB_TRANSECTID),
     TAXON_CODE = all_urchs,
     TEST_SIZE_BIN_MM = all_bins
   )
@@ -142,7 +136,8 @@ process_urchins <- function(data,
 
 
 
-  data_full$TAXON_NAME[is.na(data_full$TAXON_NAME)] <- data_full$TAXON_CODE[is.na(data_full$TAXON_NAME)]
+  data_full$TAXON_NAME[is.na(data_full$TAXON_NAME)] <-
+    data_full$TAXON_CODE[is.na(data_full$TAXON_NAME)]
 
   data_full$TAXON_NAME <- recode_factor(
     data_full$TAXON_NAME,
@@ -175,13 +170,12 @@ process_urchins <- function(data,
                     c("TRGR", "HEMA", "EUME", "MEGL")] <- "NON"
 
   # Add transect length to data frame
-  data_full$TRANSECT_LENGTH_M <-
-    summary_transect$transect_length[match(
-      data_full$CB_TRANSECTID, summary_transect$transect_id)]
 
-  data$TRANSECT_LENGTH_M <-
-    summary_transect$transect_length[match(
-      data$CB_TRANSECTID, summary_transect$transect_id)]
+  data_full$OCC_SITEID_TRANSECT <-
+    paste0(data_full$OCC_SITEID, "-", data_full$CB_TRANSECTID)
+
+  data_full$TRANSECT_LENGTH_M <-
+    summary_transect$TRANSECT_LENGTH_M[match(data_full$OCC_SITEID_TRANSECT , summary_transect$OCC_SITEID_TRANSECT)]
 
   # Remove non-eroding urchin taxa
   data_eroders <- subset(data_full, data_full$GROUP != "NON")
@@ -226,8 +220,8 @@ process_urchins <- function(data,
     ) %>%
     summarize(
       TRANSECT_LENGTH_M = mean(.data$TRANSECT_LENGTH_M),
-      TRANSECT_URCHIN_ABUNDANCE = sum(.data$COUNT),
-      TRANSECT_URCHIN_DENSITY = sum(.data$COUNT) / mean(.data$TRANSECT_LENGTH_M)
+      TRANSECT_URCHIN_ABUNDANCE_NO = sum(.data$COUNT),
+      TRANSECT_URCHIN_DENSITY_NO_M2 = sum(.data$COUNT) / mean(.data$TRANSECT_LENGTH_M)
     )
 
   # Calculate site level density
@@ -250,51 +244,51 @@ process_urchins <- function(data,
       .data$TEST_SIZE_BIN_MM
     ) %>%
     summarize(
-      SITE_URCHIN_DENSITY_MEAN = mean(.data$TRANSECT_URCHIN_DENSITY),
-      SITE_URCHIN_DENSITY_SD = sd(.data$TRANSECT_URCHIN_DENSITY)
+      SITE_URCHIN_DENSITY_NO_M2_MEAN = mean(.data$TRANSECT_URCHIN_DENSITY_NO_M2),
+      SITE_URCHIN_DENSITY_NO_M2_SD = sd(.data$TRANSECT_URCHIN_DENSITY_NO_M2)
     )
 
   # Initiate data columns
-  transect_density_group$TEST_SIZE_MEDIAN <- NA
-  transect_density_group$TRANSECT_EROSION <- NA
+  transect_density_group$TEST_SIZE_MEDIAN_MM <- NA
+  transect_density_group$TRANSECT_EROSION_G_URCHIN_DAY <- NA
 
   # Set median test size for each test size bin
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_0_20_MM"] <- 10
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_21_40_MM"] <- 30
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_41_60_MM"] <- 50
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_61_80_MM"] <- 70
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_81_100_MM"] <- 90
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_101_120_MM"] <- 110
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_121_140_MM"] <- 130
-  transect_density_group$TEST_SIZE_MEDIAN[
-    transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_141_160_MM"] <- 150
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_0_20_MM"] <-
+    10
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_21_40_MM"] <-
+    30
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_41_60_MM"] <-
+    50
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_61_80_MM"] <-
+    70
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_81_100_MM"] <-
+    90
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_101_120_MM"] <-
+    110
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_121_140_MM"] <-
+    130
+  transect_density_group$TEST_SIZE_MEDIAN_MM[transect_density_group$TEST_SIZE_BIN_MM == "TEST_SIZE_BIN_141_160_MM"] <-
+    150
 
 
   # Calculate group-specific erosion rate
   for (i in sjmisc::seq_row(transect_density_group)) {
     if (transect_density_group$GROUP[i] == "ECMA") {
-      transect_density_group$TRANSECT_EROSION[i] <- 0.0003 *
-        (transect_density_group$TEST_SIZE_MEDIAN[i] ^ 1.9671) *
-        transect_density_group$TRANSECT_URCHIN_DENSITY[i] * 365
+      transect_density_group$TRANSECT_EROSION_G_URCHIN_DAY[i] <- 0.0003 *
+        (transect_density_group$TEST_SIZE_MEDIAN_MM[i] ^ 1.9671) *
+        transect_density_group$TRANSECT_URCHIN_DENSITY_NO_M2[i] * 365
 
     }
     if (transect_density_group$GROUP[i] == "DISP-ECTH") {
-      transect_density_group$TRANSECT_EROSION[i] <- 0.000003 *
-        (transect_density_group$TEST_SIZE_MEDIAN[i] ^ 3.2887) *
-        transect_density_group$TRANSECT_URCHIN_DENSITY[i] * 365
+      transect_density_group$TRANSECT_EROSION_G_URCHIN_DAY[i] <- 0.000003 *
+        (transect_density_group$TEST_SIZE_MEDIAN_MM[i] ^ 3.2887) *
+        transect_density_group$TRANSECT_URCHIN_DENSITY_NO_M2[i] * 365
 
     }
     if (transect_density_group$GROUP[i] == "OTHER") {
-      transect_density_group$TRANSECT_EROSION[i] <- 0.00004 *
-        (transect_density_group$TEST_SIZE_MEDIAN[i] ^ 2.6025) *
-        transect_density_group$TRANSECT_URCHIN_DENSITY[i] * 365
+      transect_density_group$TRANSECT_EROSION_G_URCHIN_DAY[i] <- 0.00004 *
+        (transect_density_group$TEST_SIZE_MEDIAN_MM[i] ^ 2.6025) *
+        transect_density_group$TRANSECT_URCHIN_DENSITY_NO_M2[i] * 365
 
     }
   }
@@ -317,7 +311,7 @@ process_urchins <- function(data,
       .data$CB_METHOD,
       .data$CB_TRANSECTID,
     ) %>%
-    summarize(URCHIN_EROSION_KG_M2_YR = sum(.data$TRANSECT_EROSION) / 1000)
+    summarize(URCHIN_EROSION_KG_M2_YR = sum(.data$TRANSECT_EROSION_G_URCHIN_DAY) / 1000)
 
   transect_density_sum$OCC_SITEID_TRANSECT <-
     paste(transect_density_sum$OCC_SITEID,
@@ -325,19 +319,21 @@ process_urchins <- function(data,
           sep = "-")
 
   site_erosion <- transect_density_sum %>%
-    group_by(  .data$REGION,
-               .data$REGIONCODE,
-               .data$YEAR,
-               .data$CRUISE_ID,
-               .data$LOCATION,
-               .data$LOCATIONCODE,
-               .data$OCC_SITEID,
-               .data$OCC_SITENAME,
-               .data$LATITUDE,
-               .data$LONGITUDE,
-               .data$DEPTH_M,
-               .data$LOCALDATE,
-               .data$CB_METHOD) %>%
+    group_by(
+      .data$REGION,
+      .data$REGIONCODE,
+      .data$YEAR,
+      .data$CRUISE_ID,
+      .data$LOCATION,
+      .data$LOCATIONCODE,
+      .data$OCC_SITEID,
+      .data$OCC_SITENAME,
+      .data$LATITUDE,
+      .data$LONGITUDE,
+      .data$DEPTH_M,
+      .data$LOCALDATE,
+      .data$CB_METHOD
+    ) %>%
     summarize(
       URCHIN_EROSION_KG_M2_YR_MEAN = mean(.data$URCHIN_EROSION_KG_M2_YR),
       URCHIN_EROSION_KG_M2_YR_SD = sd(.data$URCHIN_EROSION_KG_M2_YR),
@@ -351,32 +347,33 @@ process_urchins <- function(data,
     )
 
   data <- data[c(
-  "REGION",
-  "REGIONCODE",
-  "YEAR",
-  "CRUISE_ID",
-  "LOCATION",
-  "LOCATIONCODE",
-  "OCC_SITEID",
-  "OCC_SITENAME",
-  "LATITUDE",
-  "LONGITUDE",
-  "DEPTH_M",
-  "LOCALDATE",
-  "CB_METHOD",
-  "CB_TRANSECTID",
-  "TRANSECT_LENGTH_M",
-  "URCH_OBS_TF",
-  "TAXON_NAME",
-  "TAXON_CODE",
-  "TEST_SIZE_BIN_0_20_MM",
-  "TEST_SIZE_BIN_21_40_MM",
-  "TEST_SIZE_BIN_41_60_MM",
-  "TEST_SIZE_BIN_61_80_MM",
-  "TEST_SIZE_BIN_81_100_MM",
-  "TEST_SIZE_BIN_101_120_MM",
-  "TEST_SIZE_BIN_121_140_MM",
-  "TEST_SIZE_BIN_141_160_MM")]
+    "REGION",
+    "REGIONCODE",
+    "YEAR",
+    "CRUISE_ID",
+    "LOCATION",
+    "LOCATIONCODE",
+    "OCC_SITEID",
+    "OCC_SITENAME",
+    "LATITUDE",
+    "LONGITUDE",
+    "DEPTH_M",
+    "LOCALDATE",
+    "CB_METHOD",
+    "CB_TRANSECTID",
+    "TRANSECT_LENGTH_M",
+    "URCH_OBS_TF",
+    "TAXON_NAME",
+    "TAXON_CODE",
+    "TEST_SIZE_BIN_0_20_MM",
+    "TEST_SIZE_BIN_21_40_MM",
+    "TEST_SIZE_BIN_41_60_MM",
+    "TEST_SIZE_BIN_61_80_MM",
+    "TEST_SIZE_BIN_81_100_MM",
+    "TEST_SIZE_BIN_101_120_MM",
+    "TEST_SIZE_BIN_121_140_MM",
+    "TEST_SIZE_BIN_141_160_MM"
+  )]
 
 
   if (full_summary == TRUE) {
